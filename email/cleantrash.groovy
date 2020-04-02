b183d1a7f0e6f50990bf3b22ffce6c52924a1426
vkurilo <vkurilo@griddynamics.com>
2020-04-02 21:46:14 +0400

    update repo url

--- email/cleantrash.groovy
+++ email/cleantrash.groovy
@@ -6,16 +6,18 @@ import java.util.concurrent.CancellationException
 println "hello world from groovy script"
 
 //shell('echo Hello World!')
-
-pipeline {
-    agent any
-    stages {
-        stage("pull-updates-to-dev"){
-            steps {
-                sshagent (credentials: ['sshkey']) {
-                    sh 'ssh -o StrictHostKeyChecking=no -l root 195.158.9.92 uname -a'
+@com.cloudbees.groovy.cps.NonCPS
+def call() {
+    pipeline {
+        agent any
+        stages {
+            stage("pull-updates-to-dev") {
+                steps {
+                    sshagent(credentials: ['sshkey']) {
+                        sh 'ssh -o StrictHostKeyChecking=no -l root 195.158.9.92 uname -a'
+                    }
+                    sleep 2
                 }
-                sleep 2
             }
         }
     }
