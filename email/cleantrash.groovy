c7cb054195aaf7d65bb2f76c88132d262ebb049c
vkurilo <vkurilo@griddynamics.com>
2020-04-02 21:52:16 +0400

    update repo url

--- email/cleantrash.groovy
+++ email/cleantrash.groovy
@@ -2,12 +2,16 @@ import hudson.model.*
 import hudson.AbortException
 import hudson.console.HyperlinkNote
 import java.util.concurrent.CancellationException
+import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
+import hudson.AbortException
+import hudson.model.*
+import com.cloudbees.groovy.cps.NonCPS
 
 println "hello world from groovy script"
 
 //shell('echo Hello World!')
-@com.cloudbees.groovy.cps.NonCPS
-def call() {
+@NonCPS
+def call(body) {
     pipeline {
         agent any
         stages {
