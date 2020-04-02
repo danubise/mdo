63ea3f4da4261e5863ab1b7eceb56311b1fe5ee4
vkurilo <vkurilo@griddynamics.com>
2020-04-02 20:55:05 +0400

    update repo url

---
+++ email/cleantrash.groovy
@@ -0,0 +1,6 @@
+import hudson.model.*
+import hudson.AbortException
+import hudson.console.HyperlinkNote
+import java.util.concurrent.CancellationException
+
+println "hello world from groovy script"
