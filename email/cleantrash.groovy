2302dd0672e053e6b6cfc805ae4275fbfce69485
vkurilo <vkurilo@griddynamics.com>
2020-04-02 23:20:12 +0400

    update repo url

--- email/cleantrash.groovy
+++ email/cleantrash.groovy
@@ -1,13 +1,14 @@
 import hudson.model.*
 
-// get current thread / Executor
-def thr = Thread.currentThread()
-// get current build
-def build = thr?.executable
+
+//// get current thread / Executor
+//def thr = Thread.currentThread()
+//// get current build
+//def build = thr?.executable
 
 
 // get parameters
-def parameters = build?.actions.find{ it instanceof ParametersAction }?.parameters
+def parameters = build?.actions.find { it instanceof ParametersAction }?.parameters
 parameters.each {
     println "parameter ${it.name}:"
     println it.dump()
