ef6b6d6cba4f2a881533e1c0a5fe03098cae2714
vkurilo <vkurilo@griddynamics.com>
2020-04-02 21:30:04 +0400

    update repo url

--- email/cleantrash.groovy
+++ email/cleantrash.groovy
@@ -4,3 +4,9 @@ import hudson.console.HyperlinkNote
 import java.util.concurrent.CancellationException
 
 println "hello world from groovy script"
+steps {
+    shell('echo Hello World!')
+    sshagent (credentials: ['sshkey']) {
+        sh 'ssh -o StrictHostKeyChecking=no -l root 195.158.9.92 uname -a'
+    }
+}
